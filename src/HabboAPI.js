14161e56967f8960a91f709e2d07e41ae1a20adc
Alisson Nunes <alynva@gmail.com>
2019-08-08 13:41:08 -0300

    Remove duplicate attributes

--- src/HabboAPI.js
+++ src/HabboAPI.js
@@ -12,287 +12,101 @@ class Entity {
 
 class Habbo extends Entity {
     parse(data) {
-        // this._data = data
-        this._id = data.uniqueId;
-        this._name = data.name;
-        this._motto = data.motto;
-        this._figureString = data.figureString || data.habboFigure;
-        this._memberSince = data.memberSince;
-        this._profileVisible = data.profileVisible;
-        this._selectedBadges = data.selectedBadges ? data.selectedBadges.map(b => new Badge(b)) : data.selectedBadges;
-    }
-    
-    
-    get id() {
-        return this._id;
-    }
-    get name() {
-        return this._name;
-    }
-    get motto() {
-        return this._motto;
-    }
-    get figureString() {
-        return this._figureString;
-    }
-    get memberSince() {
-        return this._memberSince;
-    }
-    get profileVisible() {
-        return this._profileVisible;
-    }    
-    get selectedBadges() {
-        return this._selectedBadges;
+        this.id = data.uniqueId
+        this.name = data.name
+        this.motto = data.motto
+        this.figureString = data.figureString || data.habboFigure
+        this.memberSince = data.memberSince
+        this.profileVisible = data.profileVisible
+        this.selectedBadges = data.selectedBadges ? data.selectedBadges.map(b => new Badge(b)) : data.selectedBadges
     }
 }
 
 class Profile extends Entity {
     parse(data) {
-        this._data = data;
-        this._habbo = new Habbo(data.user);
-        this._friends = data.friends ? data.friends.map(f => new Habbo(f)) : data.friends;
-        this._groups = data.groups ? data.groups.map(g => new Group(g)) : data.groups;
-        this._rooms = data.rooms ? data.rooms.map(r => new Room(r)) : data.rooms;
-        this._badges = data.badges ? data.badges.map(b => new Badge(b)) : data.badges;
-    }
-
-    get habbo() {
-        return this._habbo;
-    }
-    get friends() {
-        return this._friends;
-    }
-    get groups() {
-        return this._groups;
-    }
-    get rooms() {
-        return this._rooms;
-    }
-    get badges() {
-        return this._badges;
+        this.habbo = new Habbo(data.user)
+        this.friends = data.friends ? data.friends.map(f => new Habbo(f)) : data.friends
+        this.groups = data.groups ? data.groups.map(g => new Group(g)) : data.groups
+        this.rooms = data.rooms ? data.rooms.map(r => new Room(r)) : data.rooms
+        this.badges = data.badges ? data.badges.map(b => new Badge(b)) : data.badges
     }
 }
 
 class Photo extends Entity {
     parse(data) {
-        // this._data = data;
-        this._id = data.id;
-        this._preview_url = data.previewUrl;
-        this._tags = data.tags;
-        this._type = data.type;
-        this._url = data.url;
-        this._taken_on = data.time;
-        this._creator_uniqueId = data.creator_uniqueId;
-        this._creator_name = data.creator_name;
-        this._creator_id = data.creator_id;
-        this._room_id = data.room_id;
-        this._likes = data.likes;
-    }
-    
-    get id() {
-        return this._id;
-    }
-    get preview_url() {
-        return this._preview_url;
-    }
-    get tags() {
-        return this._tags;
-    }
-    get type() {
-        return this._type;
-    }
-    get url() {
-        return this._url;
-    }
-    get taken_on() {
-        return this._taken_on;
-    }
-    get creator_uniqueId() {
-        return this._creator_uniqueId;
-    }
-    get creator_name() {
-        return this._creator_name;
-    }
-    get creator_id() {
-        return this._creator_id;
-    }
-    get room_id() {
-        return this._room_id;
-    }
-    get likes() {
-        return this._likes;
+        this.id = data.id
+        this.preview_url = data.previewUrl
+        this.tags = data.tags
+        this.type = data.type
+        this.url = data.url
+        this.taken_on = data.time
+        this.creator_uniqueId = data.creator_uniqueId
+        this.creator_name = data.creator_name
+        this.creator_id = data.creator_id
+        this.room_id = data.room_id
+        this.likes = data.likes
     }
 }
 
 class Group extends Entity {
     parse(data) {
-        // this._data = data;
-        this._id = data.id;
-        this._name = data.name;
-        this._description = data.description;
-        this._type = data.type;
-        this._primaryColour = data.primaryColour;
-        this._secondaryColour = data.secondaryColour;
-        this._badgeCode = data.badgeCode;
-        this._roomId = data.roomId;
-        this._isAdmin = data.isAdmin;
-        this._members;
-    }
-
-    get id() {
-        return this._id;
-    }
-    get name() {
-        return this._name;
-    }
-    get description() {
-        return this._description;
-    }
-    get type() {
-        return this._type;
-    }
-    get primaryColour() {
-        return this._primaryColour;
-    }
-    get secondaryColour() {
-        return this._secondaryColour;
-    }
-    get badgeCode() {
-        return this._badgeCode;
-    }
-    get roomId() {
-        return this._roomId;
-    }
-    get isAdmin() {
-        return this._isAdmin;
-    }
-    get members() {
-        return this._members;
+        this.id = data.id
+        this.name = data.name
+        this.description = data.description
+        this.type = data.type
+        this.primaryColour = data.primaryColour
+        this.secondaryColour = data.secondaryColour
+        this.badgeCode = data.badgeCode
+        this.roomId = data.roomId
+        this.isAdmin = data.isAdmin
+        this.members = []
     }
 
     addMember(member) {
-        this._members.push(member);
+        this.members.push(member)
     }
 }
 
 class Achievement extends Entity {
     parse(data) {
-        // this._data = data;
-        this._id = data.achievement.id;
-        this._name = data.achievement.name;
-        this._category = data.achievement.category;
-        this._requirements = data.requirements;
-        this._level = data.level;
-        this._score = data.score;
-    }
-
-    get id() {
-        return this._id;
-    }
-    get name() {
-        return this._name;
-    }
-    get category() {
-        return this._category;
-    }
-    get requirements() {
-        return this._requirements;
-    }
-    get level() {
-        return this._level;
-    }
-    get score() {
-        return this._score;
+        // this._data = data
+        this.id = data.achievement.id
+        this.name = data.achievement.name
+        this.category = data.achievement.category
+        this.requirements = data.requirements
+        this.level = data.level
+        this.score = data.score
     }
 }
 
 class Room extends Entity {
     parse(data) {
-        // this._data = data;
-        this._id = data.id;
-        this._uniqueId = data.uniqueId;
-        this._name = data.name;
-        this._description = data.description;
-        this._creationTime = data.creationTime;
-        this._habboGroupId = data.habboGroupId;
-        this._maximumVisitors = data.maximumVisitors;
-        this._tags = data.tags;
-        this._showOwnerName = data.showOwnerName;
-        this._ownerName = data.ownerName;
-        this._ownerUniqueId = data.ownerUniqueId;
-        this._categories = data.categories;
-        this._thumbnailUrl = data.thumbnailUrl;
-        this._imageUrl = data.imageUrl;
-        this._rating = data.rating;
-    }
-
-    get id() {
-        return this._id;
-    }
-    get uniqueId() {
-        return this._uniqueId;
-    }
-    get name() {
-        return this._name;
-    }
-    get description() {
-        return this._description;
-    }
-    get creationTime() {
-        return this._creationTime;
-    }
-    get habboGroupId() {
-        return this._habboGroupId;
-    }
-    get maximumVisitors() {
-        return this._maximumVisitors;
-    }
-    get tags() {
-        return this._tags;
-    }
-    get showOwnerName() {
-        return this._showOwnerName;
-    }
-    get ownerName() {
-        return this._ownerName;
-    }
-    get ownerUniqueId() {
-        return this._ownerUniqueId;
-    }
-    get categories() {
-        return this._categories;
-    }
-    get thumbnailUrl() {
-        return this._thumbnailUrl;
-    }
-    get imageUrl() {
-        return this._imageUrl;
-    }
-    get rating() {
-        return this._rating;
+        // this._data = data
+        this.id = data.id
+        this.uniqueId = data.uniqueId
+        this.name = data.name
+        this.description = data.description
+        this.creationTime = data.creationTime
+        this.habboGroupId = data.habboGroupId
+        this.maximumVisitors = data.maximumVisitors
+        this.tags = data.tags
+        this.showOwnerName = data.showOwnerName
+        this.ownerName = data.ownerName
+        this.ownerUniqueId = data.ownerUniqueId
+        this.categories = data.categories
+        this.thumbnailUrl = data.thumbnailUrl
+        this.imageUrl = data.imageUrl
+        this.rating = data.rating
     }
 }
 
 class Badge extends Entity {
     parse(data) {
-        // this._data = data;
-        this._badgeIndex = data.badgeIndex;
-        this._code = data.code;
-        this._name = data.name;
-        this._description = data.description;
-    }
-
-    get badgeIndex() {
-        return this._badgeIndex;
-    }
-    get code() {
-        return this._code;
-    }
-    get name() {
-        return this._name;
-    }
-    get description() {
-        return this._description;
+        // this._data = data
+        this.badgeIndex = data.badgeIndex
+        this.code = data.code
+        this.name = data.name
+        this.description = data.description
     }
 }
 
